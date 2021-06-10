0a05dfb9667ba1154bd34daf224236830eef19fb
Ye Shi <zoe.shiye@gmail.com>
2021-06-10 20:34:51 +1000

    Add: express and body-parser

--- calculator.js
+++ calculator.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const num1 = Number(req.body.n1);
+  const num2 = Number(req.body.n2);
+  const result = num1 + num2;
+  res.send("The result of the calculation is " + result);
 });
 
 app.listen(port, () => {
